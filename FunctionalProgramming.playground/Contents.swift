718f43d050f199fd4e4d454763096fb803fc531f
Kevin Ho <keho98@gmail.com>
2015-10-29 17:18:43 -0700

    Updates functional programming

--- FunctionalProgramming.playground/Contents.swift
+++ FunctionalProgramming.playground/Contents.swift
@@ -20,14 +20,75 @@ let sortedNumber = numbers.sort(sortAscending)
 /* 
  Expression syntax
  { (parameters) -> ReturnType in
-
+    CODE_HERE
  }
 
 */
 
-numbers = [4,2,6,7,10,2]
+numbers = [4,2,6,7,10,12]
 
 numbers.sort {
     (i, j) -> Bool in
     return i < j
 }
+
+// A nice alternative
+
+let result = numbers.sort { $0 > $1 }
+result
+
+// Also wtf
+// '<' is actually just a function that returns a bool
+let wtfResult = numbers.sort(<)
+
+wtfResult
+
+// Map, can work between types as well
+
+let numbersAsStrings = numbers.map {
+    (i) -> String in
+    return i.description
+}
+
+numbersAsStrings
+
+let mappedNumber = numbers.map {
+    (i) -> Int in
+    return i * 3
+}.reduce(0, combine: +)
+
+mappedNumber
+
+// Functions can return other functions
+
+func printLineMaker(times: Int) -> String -> () {
+    func printer(text: String) {
+        for _ in 1...times {
+            print(text)
+        }
+    }
+    return printer
+}
+
+let repeatPrintLn = printLineMaker(3)
+repeatPrintLn("Yay!")
+
+// Challenge - Implement Map and Filter using reduce
+
+var inputArray: [Int] = []
+
+numbers.reduce(inputArray) {
+    (var array: [Int], i) -> [Int] in
+    array.append(i * 3)
+    return array
+}
+
+func map<T, U>(arr: [T], handler: T -> U) -> [U] {
+    return arr.reduce([]) { (var tempArr, val) -> [U] in
+        tempArr.append(handler(val))
+        return tempArr
+    }
+}
+
+map(numbers) { $0 * 3 }
+
